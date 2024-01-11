ce576047bccabfe68e5cbb0fb070e4d258b11108
Personal <soumwadeep@gmail.com>
2024-01-11 06:50:38 +0530

    Initial

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,10 @@
+import NavBar from "@/components/Navbar";
+
 const Home = () => {
   return (
     <div>
-      <h1 className="text-center text-xl">Welcome To Cineflix</h1>
+      <NavBar />
+      <h1>Hello!</h1>
     </div>
   );
 };
